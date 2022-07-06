a65349bdd24a5f7681fdac7ac038a15001d71a7c
syrgak769 <syrgak.first@gmail.com>
2022-07-06 09:05:50 +0300

    import project

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./assets/css/style.css">
+    <title>Document</title>
+</head>
+<body>
+    <div class="wrapper">
+
+        <div class="menu container">
+            <div class="topCard">
+                <div class="topCard__img"> <img src="./assets/img/breakFast.jpg" alt=""> </div>
+                <div class="topCard__texts">
+                    <p class="topCard__texts-title"> <span class="topCard__texts-title--weight">Ex tempor euismod sapientem ouis lexor pro.</span> Ad vix dicta iudico singulis, ius ex mollis veritus.</p>
+                    <p class="topCard__texts-subTitle">Possit tritani consectetuer eu his, nusquam minimum ut per, eu eos ocurreret splendide hendrerit. Dicant placerat democritum eos id, sit mollis invidunt mediocritatem ei, ne fuisset dolores periculis sed.</p>
+                </div>
+            </div>
+
+            <div class="bottomCard">
+                <div class="bottomCard__texts">
+                    <p class="bottomCard__texts-title"> <span class="bottomCard__texts-title--weight">Te elit antiopam vix zril recusabo ut pri.</span>Quas suscipit ad mea verear vivendo tincidunt.</p>
+                    <p class="bottomCard__texts-subTitle">Pri cu dico labores officiis, odio principes complectitur ad sea. Sea id doctus forensibus, nec lorem vocent aliquam eu. Aliquid definitiones id cum, ad meliore perpetua referrentur sed. Quas suscipit ad mea verear vivendo tincidunt.</p>
+                </div>
+
+                <div class="bottomCard__img"> <img src="./assets/img/dessert.jpg" alt=""> </div>
+            </div>
+        </div>
+
+
+    </div>
+</body>
+</html>
